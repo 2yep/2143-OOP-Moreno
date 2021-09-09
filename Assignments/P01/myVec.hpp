47f0211f6f69a31704d93340da65fbec17458faa
2yep <miklosam1999@gmail.com>
2021-09-09 18:07:44 -0500

    ew

--- Assignments/P01/myVec.hpp
+++ Assignments/P01/myVec.hpp
@@ -5,17 +5,19 @@
 
 using namespace std;
 
-// Node for our linked list
+// node for our linked list
 struct node
 {
     int data; // data value (could be a lot more values)
 
     node *next; // we always need a "link" in a linked list
+    node *previous;
 
     node(int x)
     { // cunstructor to make adding values easy
         data = x;
         next = NULL;
+        previous = NULL;
     }
 };
 
@@ -24,39 +26,32 @@ class MyVector
 private:
     node *head;
     node *tail;
-    int size;
+    int size = 0;
 
 public:
-    // int &operator[](int index, const MyVector other)
-    // {
-    //  }
-
     MyVector()
     {
-        head = nullptr;
+        head = tail = NULL;
     }
 
-    MyVector(string FileName, int *&arr, int &size)
+    MyVector(string FileName)
     {
-        ifstream fin;        // stream reference
-                             //
-        fin.open(FileName);  // open the file
-                             //
-        fin >> size;         // get first value which contains
-                             // number of remaining values in file
-                             //
-        arr = new int[size]; // allocate new array of correct size
-                             //
-        int i = 0;           // need an array index for our while loop
-                             //
+        ifstream fin;       // stream reference
+                            //
+        fin.open(FileName); // open the file
+                            //
+        fin >> size;        // get first value which contains
+                            // file size
+        int dat;
         // Can also use for loop since we know the exact count in file.
         // eof = end of file flag
         // `!fin.eof()` evaulates to true when we hit end of file.
         while (!fin.eof())
         {
-            fin >> arr[i]; // read file value straight into array
-                           // at index i
-            i++;           // increment array index
+            fin >> dat;
+            pushFront(dat); // push dat onto list
+
+            size++; // size index ++
         }
     }
 
@@ -73,81 +68,235 @@ public:
 
     MyVector(const MyVector &V1)
     {
+        head = tail = NULL;
+        node *temp = V1.head;
+        while (temp)
+        {
+            pushRear(temp->data);
+            temp = temp->next;
+        }
     }
 
     void pushFront(int x)
     {
-        node *tempPtr = new node(x); // create a new node and
-                                     // add data to it
+        node *temp = new node(x); // create a new node and
+                                  // add data to it
 
         if (!head)
         { // `!head` implies empty list
             // So does `head == NULL`
 
-            head = tail = tempPtr; // `head = tempPtr` places addrress of
-                                   // tempPtr in head (points head to tempPtr)
+            head = tail = temp; // `head = tempPtr` places addrress of
+                                // tempPtr in head (points head to tempPtr)
         }
         else
-        {                         // list not empty
-            tempPtr->next = head; // point tempPtr's next to what head points to
-            head = tempPtr;       // now point head to tempPtr
+        {                      // list not empty
+            temp->next = head; // point tempPtr's next to what head points to
+            head = temp;       // now point head to tempPtr
         }
     }
 
     void pushFront(MyVector V2)
     {
-        for (int i = 0; i < V2.size; i++)
+        node *temp = V2.tail;
+
+        while (temp)
         {
-            pushFront(V2.tail->data);
+            pushRear(temp->data);
+            temp = temp->previous;
         }
     }
 
     void pushRear(int x)
     {
-        node *tempPtr = new node(x); // create a new node and
-                                     // add data to it
+        node *temp = new node(x); // create a new node and
+                                  // add data to it
         if (!head)
         { // `!head` implies empty list
             // So does `head == NULL`
 
-            head = tail = tempPtr; // `head = tempPtr` places addrress of
-                                   // tempPtr in head (points head to tempPtr)
+            head = tail = temp; // `head = tempPtr` places addrress of
+                                // tempPtr in head (points head to tempPtr)
         }
         else
         {
-            tail->next = tempPtr; // point tempPtr's next to what head points to
-            tail = tempPtr;       // now point head to tempPtr
+            tail->next = temp; // point tempPtr's next to what head points to
+            tail = temp;       // now point head to tempPtr
         }
     }
 
     void pushRear(MyVector V2)
     {
-        for (int i = 0; i < V2.size; i++)
+        node *temp = V2.head;
+
+        while (temp)
         {
-            pushRear(V2.head->data);
+            pushRear(temp->data); // print data from node
+            temp = temp->next;    // move to next node
         }
     }
 
     int popFront()
     {
         int bruh = head->data;
+        node *temp = head;
+        head = head->next;
+        delete temp;
         return bruh;
     }
 
     int popRear()
     {
         int bruh = tail->data;
+        node *temp = tail;
+        tail = tail->previous;
+        delete temp;
         return bruh;
+    }
+
+    void PushAt(int loc, int val)
+    {
+        node *temp = head;
+        node *newNode;
+        newNode->data = val;
+
+        int tempPos = 0; // Traverses through the list
+
+        temp = head; // Initialize tempent to head;
+        if (head != NULL)
+        {
+            while (temp->next != NULL && tempPos != loc)
+            {
+                temp->previous = temp;
+                temp = temp->next;
+                tempPos++;
+            }
+            if (loc == 0)
+            {
+                pushFront(val);
+            }
+            else if (temp->next == NULL && loc == tempPos + 1)
+            {
+                pushRear(val);
+            }
+            else if (loc > tempPos + 1)
+                cout << "location does not exist" << endl;
+            else
+            {
+                temp->next = newNode;
+                newNode->next = temp;
+            }
+        }
+    }
+
+    void inOrderPush(int val)
+    {
+        node *temp = node(val);
+        if (!head)
+        {
+            head = tail = temp;
+        }
+        else
+        {
+            if (temp->value < head->value)
+            {
+                //frontsert
+                temp->next = head;
+                head = temp;
+            }
+            else if (temp->value > tail->value)
+            {
+                //endsert
+                tail->next = temp;
+                tail = temp;
+            }
+            else
+            {
+                //find the location
+                node *Prev = head;
+                node *Curr = head;
+                while (Curr->value < temp->value)
+                {
+                    Prev = Curr;
+                    Curr = Curr->next;
+                }
+                Prev->next = temp;
+                temp->next = Curr;
+            }
+        }
+    }
+
+    int popAt(int loc)
+    {
+        node *temp = head int pop;
+
+        int tempPos = 0; // Traverses through the list
+
+        // Initialize tempent to head;
+        if (head != NULL)
+        {
+            while (temp->next != NULL && tempPos != loc)
+            {
+                temp->previous = temp;
+                temp = temp->next;
+                tempPos++;
+            }
+            // if(loc==0)
+            // {
+            //    pushFront(val);
+            // }
+            // else if(temp->next == NULL && loc == tempPos+1)
+            // {
+            //  pushRear(val);
+            // }
+            if (loc > tempPos + 1)
+                cout << "location does not exist" << endl;
+            else
+            {
+                pop = temp->data;
+                node *temp2 = temp;
+                temp = temp->previous;
+                delete temp;
+                return pop;
+            }
+        }
+    }
+
+    int find(int val)
+    {
+        node *temp = head;
+        int i = 0;
+
+        while (temp)
+        {
+            if (val == temp->data)
+            {
+                return i;
+            }
+
+            temp = temp->next;
+            i++
+        }
+        return -1
     }
 
     void print()
     {
-        cout << "[";
-        node *temp = head;
+        node *temp = head; // temp pointer copies head
+
         while (temp)
-        {
-            cout << temp->data << ", ";
+        { // this loops until temp is NULL
+            // same as `while(temp != NULL)`
+            cout << "[";
+            cout << temp->data; // print data from node
+            if (temp->next)
+            {
+                cout << ", ";
+            }
+            temp = temp->next; // move to next node
         }
-        cout << "]" << endl;
+        cout << "]";
+        cout << endl;
     }
-};
+}
+}
